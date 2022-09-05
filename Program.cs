c7c6907fe19bf13033ba5eeb9c561b4cb8d86f2f
Assauit <maxst01@list.ru>
2022-09-05 09:45:44 +0700

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -1,28 +1,52 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-       {
- 
-        int a, b;
- 
-        Console.WriteLine("Введите два числа : ");
- 
-        a = Convert.ToInt32(Console.ReadLine());
- 
-        b = Convert.ToInt32(Console.ReadLine());
- 
-        if (a > b) 
-        {
- 
-            Console.WriteLine("{0} наибольшее число", a);
- 
-        } 
-        else 
-        {
- 
-            Console.WriteLine("{0} наибольшее число ", b);
- 
-        } 
-        Console.ReadLine();
-     }
-}
+﻿//internal class Program
+//{
+//    private static void Main(string[] args)
+//       {
+// 
+//        int a, b;
+// 
+//        Console.WriteLine("Введите два числа : ");
+// 
+//        a = Convert.ToInt32(Console.ReadLine());
+// 
+//        b = Convert.ToInt32(Console.ReadLine());
+// 
+//       if (a > b) 
+//        {
+// 
+//            Console.WriteLine("{0} наибольшее число", a);
+// 
+//        } 
+//        else 
+//        {
+// 
+//            Console.WriteLine("{0} наибольшее число ", b);
+// 
+//       } 
+//        Console.ReadLine();
+//     }
+//}
+
+
+
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        int a, b, c;
+//        Console.WriteLine("Введите три числа : ");
+//        a = Convert.ToInt32(Console.ReadLine());
+//        b = Convert.ToInt32(Console.ReadLine());
+//        c = Convert.ToInt32(Console.ReadLine());
+//        int max = a;
+//        if (b > a) {
+//           max = b;
+//        }
+//        if (c > b){
+//            max = c;
+//        }
+//        Console.WriteLine("Максимальное: " +max);
+//
+//        }
+//}
+
